0d871e31f7dffcfa8496a914216b46a98b03b872
Josh Duffney <jduffney@microsoft.com>
2022-11-16 21:41:11 +0000

    add exec command with setup action

--- index.js
+++ index.js
@@ -4,14 +4,11 @@ const core = require('@actions/core');
 try {
     // `image-name` input defined in action metadata file
     const imageName = core.getInput('image-name');
-    console.log(imageName);
     const envelopeType = core.getInput('envolope-type');
-    console.log(envelopeType);
     const mediaType = core.getInput('media-type');
-    console.log(mediaType);
     const cmd = 'notation sign'
     let array = [];
-    
+
     array.push(cmd)
 
     if (envelopeType != null) {
@@ -23,13 +20,12 @@ try {
     
     array.push(imageName)
     let result = array.join(' ');
-    console.log(result)
 
-    core.setOutput(result);
+    const output = execSync(result, { encoding: 'utf-8' });
+    console.log('Ran command:', output);
 
-    // Get the JSON webhook payload for the event that triggered the workflow
-    //const payload = JSON.stringify(github.context.payload, undefined, 2)
-    //console.log(`The event payload: ${payload}`);
+    //core.setOutput(result);
+
 } catch (error) {
     core.setFailed(error.message);
 }
