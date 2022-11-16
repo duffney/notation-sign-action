484aa3da5c66bc4b72c4de34dab0446bac7e1a08
Josh Duffney <jduffney@microsoft.com>
2022-11-16 21:43:30 +0000

    recompile

--- dist/index.js
+++ dist/index.js
@@ -2823,14 +2823,11 @@ const core = __nccwpck_require__(129);
 try {
     // `image-name` input defined in action metadata file
     const imageName = core.getInput('image-name');
-    console.log(imageName);
     const envelopeType = core.getInput('envolope-type');
-    console.log(envelopeType);
     const mediaType = core.getInput('media-type');
-    console.log(mediaType);
     const cmd = 'notation sign'
     let array = [];
-    
+
     array.push(cmd)
 
     if (envelopeType != null) {
@@ -2842,13 +2839,12 @@ try {
     
     array.push(imageName)
     let result = array.join(' ');
-    console.log(result)
 
-    core.setOutput(result);
+    const output = execSync(result, { encoding: 'utf-8' });
+    console.log('Ran command:', output);
 
-    // Get the JSON webhook payload for the event that triggered the workflow
-    //const payload = JSON.stringify(github.context.payload, undefined, 2)
-    //console.log(`The event payload: ${payload}`);
+    //core.setOutput(result);
+
 } catch (error) {
     core.setFailed(error.message);
 }
